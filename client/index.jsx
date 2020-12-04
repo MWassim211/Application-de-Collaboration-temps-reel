cae22916845ae35620bd48551266f9d16ead82a7
Wassim <mohamedwassil.beldjillali@gmail.com>
2020-12-04 11:24:37 +0100

    set up TP3

--- client/index.jsx
+++ client/index.jsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Header from './components/Header/index';
-import Content from './components/Content/index';
 
 const Index = () => (
   <div className="container">
-    <Header />
-    <Content />
+    Hello
   </div>
 );
 ReactDOM.render(<Index />, document.getElementById('root'));
