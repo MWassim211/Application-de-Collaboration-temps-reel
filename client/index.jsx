4a6add40403e2c7929e59a08f63bc79fc406348b
Wassim <mohamedwassil.beldjillali@gmail.com>
2020-12-08 19:54:50 +0100

    set up home Page

--- client/index.jsx
+++ client/index.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  HashRouter as Router,
+  Route,
+  Switch,
+} from 'react-router-dom';
 import DataChat from './components/DataChat';
+import HomePage from './components/HomePage';
 
 const Index = () => (
-  <div className="container">
-    <DataChat />
-  </div>
+  <Router>
+    <div className="container">
+      <HomePage />
+      <Switch>
+        <Route exact path="/chat">
+          <DataChat />
+        </Route>
+        <Route exact path="/video">
+          <p> hello </p>
+        </Route>
+      </Switch>
+    </div>
+  </Router>
 );
 ReactDOM.render(<Index />, document.getElementById('root'));
