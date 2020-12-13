4acc0262a5fecab3c163872056c2765aa7dfe101
Michelle LEANO MARTINET <michelle.leano-martinet@etu.univ-lyon1.fr>
2020-12-13 23:15:54 +0100

    WhatsApp be careful...

--- client/config/PeerConfig.js
+++ client/config/PeerConfig.js
@@ -1,7 +1,7 @@
 export default {
-  host: 'localhost',
-  port: 3000,
-  // host: 'tiw8-chat.herokuapp.com',
+  // host: 'localhost',
+  // port: 3000,
+  host: 'tiw8-chat.herokuapp.com',
   path: '/mypeer',
   debug: 2,
 };
